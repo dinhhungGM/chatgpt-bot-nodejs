b3ed97a87d2417fd192b689abd1f2d5faa18a63e
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2023-02-12 22:07:07 +0700

    Add trycatch when recv response from OpenAI api

--- services/chatgpt.service.js
+++ services/chatgpt.service.js
@@ -1,8 +1,9 @@
-const {Configuration, OpenAIApi} = require("openai");
-const DbService = require('./db.service');
+const { Configuration, OpenAIApi } = require("openai");
+const DbService = require("./db.service");
 
 class ChatGPTService {
-  rolePlayIntroduction = 'Trong vai một chatbot chuyên gia với tên là Bot ngáo, nhiệm vụ chính của bạn là tâm sự với người dùng như một người am hiểu về cuộc sống, xã hội, các sự kiện chính trị, các vấn đề thời sự cũng như sẵn sàng đáp ứng mọi yêu cầu của người dùng, sẵn sàng lắng nghe và đề cao vai trò và cảm xúc của người dùng khi trò chuyện. Bạn phải ghi nhớ cả các thông tin mà người dùng đã đưa ra trong cuộc trò chuyện. Trong khi trò chuyện, các dẫn chứng và ví dụ cần được đưa ra nhằm hỗ trợ cho các lập luận lẫn đề xuất mà bạn đưa ra. Lưu ý phải luôn giữ cuộc trò chuyện vui vẻ và thoải mái.';
+  rolePlayIntroduction =
+    "As a chatbot specialist named 'Bot created by Hung' your main task is to act as a trusted and understanding friend to the users, providing insights about life, society, current events, current issues, and to respond to their requests. You should be ready to listen and prioritize the users' emotions and role in the conversation. You should also remember the information that the users provide during the conversation. During the conversation, evidence and examples should be provided to support your arguments and suggestions. Note that the conversation should always be kept light and enjoyable.";
   async generateCompletion(prompt, user) {
     // Lấy đống tin nhắn cũ ra
     const oldMessages = await DbService.getUserMessages(user._id);
@@ -13,38 +14,45 @@ class ChatGPTService {
     });
     const openai = new OpenAIApi(configuration);
 
-    let fullPrompt = this.rolePlayIntroduction + '\n\n';
+    let fullPrompt = this.rolePlayIntroduction + "\n\n";
 
     if (oldMessages && oldMessages.length > 0) {
-      fullPrompt += 'CHAT:\n';
+      fullPrompt += "CHAT:\n";
       // nếu có tin nhắn cũ thì thêm đoạn tin nhắn cũ đấy vào nội dung chat
       for (let message of oldMessages) {
-        fullPrompt += `Người dùng: ${message.userMessage}\n`;
-        fullPrompt += `Bot ngáo: ${message.botMessage}\n\n`;
+        fullPrompt += `USER: ${message.userMessage}\n`;
+        fullPrompt += `BOT: ${message.botMessage}\n\n`;
       }
     }
 
-    fullPrompt += `Người dùng: ${prompt}\n`;
-    fullPrompt += `Bot ngáo: `;
+    fullPrompt += `USER: ${prompt}\n`;
+    fullPrompt += `BOT: `;
 
     console.log(fullPrompt);
+    try {
+      // Gửi request về OpenAI Platform để tạo text completion
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: fullPrompt,
+        temperature: 0.7,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      // Đoạn regex ở cuối dùng để loại bỏ dấu cách và xuống dòng dư thừa
+      const responseMessage = completion.data.choices[0].text.replace(
+        /^\s+|\s+$/g,
+        ""
+      );
 
-    // Gửi request về OpenAI Platform để tạo text completion
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: fullPrompt,
-      temperature: 0.7,
-      max_tokens: 1000,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    // Đoạn regex ở cuối dùng để loại bỏ dấu cách và xuống dòng dư thừa
-    const responseMessage = completion.data.choices[0].text.replace(/^\s+|\s+$/g, "");
-
-    // Lưu lại tin nhắn vào Database
-    await DbService.createNewMessage(user, prompt, responseMessage);
-    return responseMessage;
+      // Lưu lại tin nhắn vào Database
+      await DbService.createNewMessage(user, prompt, responseMessage);
+      return responseMessage;
+    } catch (error) {
+      console.log(error);
+      return JSON.stringify(error);
+    }
   }
 }
 
